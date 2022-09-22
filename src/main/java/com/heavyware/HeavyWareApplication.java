283f44a647eab42844e491aa7fcdfcf48a12d891
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-09-22 17:30:03 +0300

    spring init

---
+++ src/main/java/com/heavyware/HeavyWareApplication.java
@@ -0,0 +1,13 @@
+package com.heavyware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavyWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavyWareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heavyware/HeavyWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heavyware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavyWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
