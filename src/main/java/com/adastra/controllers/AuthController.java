99627c24db9878c8a495a30d516fc5e2458385d0
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-10-06 16:28:37 +0300

    navbar login and register templates

---
+++ src/main/java/com/adastra/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.adastra.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegister() {
+        return "register";
+    }
+}
