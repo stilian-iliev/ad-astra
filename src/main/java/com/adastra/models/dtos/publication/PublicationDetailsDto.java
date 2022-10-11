47738ea6a8e0d5d88fd64c49443b2e44d45b093c
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-10-11 06:54:21 +0300

    user and image points

--- src/main/java/com/adastra/models/dtos/publication/PublicationDetailsDto.java
+++ src/main/java/com/adastra/models/dtos/publication/PublicationDetailsDto.java
@@ -3,12 +3,14 @@ package com.adastra.models.dtos.publication;
 import com.adastra.models.Publication;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class PublicationDetailsDto {
     private String title;
     private String description;
     private String image;
     private String user;
+    private UUID userId;
 
     private LocalDateTime date;
 
@@ -17,6 +19,7 @@ public class PublicationDetailsDto {
         this.description = publication.getDescription();
         this.image = publication.getImage();
         this.user = publication.getUser().getUsername();
+        this.userId = publication.getUser().getId();
         this.date = publication.getPublicationTime();
     }
 
@@ -34,6 +37,10 @@ public class PublicationDetailsDto {
 
     public String getUser() {
         return user;
+    }
+
+    public UUID getUserId() {
+        return userId;
     }
 
     public LocalDateTime getDate() {
